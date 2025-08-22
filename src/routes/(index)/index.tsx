96fdb5295108a063062c5dca9712863c6a1fb8e9
Kaitowing <pizzolatti.arthur@edu.pucrs.br>
2025-08-22 16:50:21 -0300

    Init Repo Correctly

--- src/routes/(index)/index.tsx
+++ src/routes/(index)/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import logo from '@/logo.svg'
 
 export const Route = createFileRoute('/(index)/')({
   component: App,
@@ -9,6 +10,7 @@ function App() {
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
         <img
+          src={logo}
           className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
           alt="logo"
         />
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
@@ -8,8 +10,12 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
-
+    tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
