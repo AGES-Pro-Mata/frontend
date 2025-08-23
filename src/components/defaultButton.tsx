2ec6adaee249361d0de157df84716ce1e161dde5
Carol <carolina.silva@sap.com>
2025-08-23 14:09:04 -0300

    feat(component): criar DefaultButton com variantes primária e secundária

--- src/components/defaultButton.tsx
+++ src/components/defaultButton.tsx
@@ -11,11 +11,10 @@ export function DefaultButton({
   variant = "primary",
   onClick,
 }: DefaultButtonProps) {
-  const base = "shadow-xs transition-none"; // sem animação
+  const base = "shadow-xs transition-none";
 
   const styles = {
     primary:
-      // cor fixa no normal e no hover
       "bg-[#4C9613] text-white hover:!bg-[#4C9613] hover:!text-white",
     secondary:
       "bg-[#D6CCC2] text-zinc-900 border border-[#D6CCC2] " +
