f6fca4134a868ccf14b05c656ebcd12ba6edb73b
Henrique-Schultz <valente.schultz@gmail.com>
2025-09-05 00:21:55 -0300

    componente carousel

--- src/components/ui/svgs/arrow.tsx
+++ src/components/ui/svgs/arrow.tsx
@@ -1,4 +1,9 @@
-export const ArrowSVG = () => {
+type ArrowProps = {
+  direction?: "left" | "right";
+  onClick?: () => void;
+};
+
+export const ArrowSVG = ({ direction = "right", onClick }: ArrowProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,15 +11,23 @@ export const ArrowSVG = () => {
       height="48"
       viewBox="0 0 48 48"
       fill="none"
+      className={`${direction === "left" ? "rotate-180" : ""} cursor-pointer`}
+      onClick={onClick}
     >
-      <rect width="48" height="48" rx="24" fill="#AFAFAF" fill-opacity="0.5" />
+      <rect
+        width="48"
+        height="48"
+        rx="24"
+        className="fill-gray-400 hover:fill-gray-500 transition-colors duration-200"
+        fillOpacity="0.5"
+      />
       <path
         d="M10 24H38M38 24L24 10M38 24L24 38"
         stroke="#F5F5F5"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
-}
+};
