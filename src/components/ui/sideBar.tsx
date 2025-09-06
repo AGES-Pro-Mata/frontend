1ccbfeb1a120452d0918cd5b15a74c640185fec3
Kaitowing <pizzolatti.arthur@edu.pucrs.br>
2025-09-06 03:25:57 -0300

    feat: Add dynamic user routes and update sidebar button matching

--- src/components/ui/sideBar.tsx
+++ src/components/ui/sideBar.tsx
@@ -778,7 +778,11 @@ export function SidebarButton({ icon, label, to }: SidebarButtonProps) {
   const matchRoute = useMatchRoute();
   return (
     <SidebarMenuItem>
-      <SidebarMenuButton size="lg" asChild isActive={!!matchRoute({ to: to })}>
+      <SidebarMenuButton
+        size="lg"
+        asChild
+        isActive={!!matchRoute({ to: to, fuzzy: true })}
+      >
         <Link to={to}>
           <span className="[&>*]:h-5 [&>*]:w-5 text-black">{icon}</span>
           <Typography variant="body" className="text-black">
