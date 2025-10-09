adf2f0cf7fc592681c943a1ff74aa21e56a3d6e6
Pedro Widholzer <pedro.widholzer@edu.pucrs.br>
2025-10-09 01:39:47 -0300

    comentado o trecho de codigo responsavel pelo timeout

--- src/hooks/use-fetch-admin-experiences.ts
+++ src/hooks/use-fetch-admin-experiences.ts
@@ -17,7 +17,7 @@ export const useFetchAdminExperiences = ({ filters }: useFetchAdminExperiencesPa
   const { data, isFetching, refetch, isLoading } = useQuery({
     queryKey: [ADMIN_EXPERIENCES_QUERY_KEY, filters],
     queryFn: async () => {
-      await new Promise(r => setTimeout(r, 5000));
+      //await new Promise(r => setTimeout(r, 5000));
       const response = await api.get<
         {
           items: TExperienceAdminResponse[];
