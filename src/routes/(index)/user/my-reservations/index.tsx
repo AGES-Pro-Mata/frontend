61d692cd168a643d1117d7c67e48b1a010cfd938
Kayky Belleboni Casagrande <kayky.casagrande@e-unicred.com.br>
2025-10-28 10:05:54 -0300

    lint

--- src/routes/(index)/user/my-reservations/index.tsx
+++ src/routes/(index)/user/my-reservations/index.tsx
@@ -1,11 +1,11 @@
 import ReservaCard from "@/components/card/myReservation";
 import { MyReservationsFilterCompact } from "@/components/filter/MyReservationsFilterCompact";
 import {
-  useMyReservations,
   type ReservationGroupStatusFilter,
+  useMyReservations,
 } from "@/hooks/useMyReservations";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState, type Key } from "react";
+import { type Key, useState } from "react";
 import { MoonLoader } from "react-spinners";
 
 export const Route = createFileRoute("/(index)/user/my-reservations/")({
@@ -16,7 +16,6 @@ function RouteComponent() {
   const [status, setStatus] = useState<ReservationGroupStatusFilter>("ALL");
 
   const { data, isFetching } = useMyReservations(status);
-  console.log(data);
 
   return (
     <main className="mx-auto flex w-full max-w-[1180px] flex-col gap-12 px-4 py-10 md:px-8">
